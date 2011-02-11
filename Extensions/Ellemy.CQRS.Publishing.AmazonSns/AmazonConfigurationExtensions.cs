f527f3da86067d194e3b730b53aad0b12252e298
Elliott O'Hara <elliott.ohara@gmail.com>
2011-02-10 22:21:35 -0600

    http://blog.elliottohara.com/2011/02/ellemycqrs-goes-to-cloud-amazon-sns.html

--- Extensions/Ellemy.CQRS.Publishing.AmazonSns/AmazonConfigurationExtensions.cs
+++ Extensions/Ellemy.CQRS.Publishing.AmazonSns/AmazonConfigurationExtensions.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Ellemy.CQRS.Config;
+
+namespace Ellemy.CQRS.Publishing.AmazonSns
+{
+    public static class AmazonConfigurationExtensions
+    {
+        public static AmazonPublisherConfig AmazonPublisher(this Configuration configuration)
+        {
+            return new AmazonPublisherConfig(configuration);
+        }
+    }
+}
--- Extensions/Ellemy.CQRS.Publishing.AmazonSns/AmazonPublisher.cs
+++ Extensions/Ellemy.CQRS.Publishing.AmazonSns/AmazonPublisher.cs
@@ -2,61 +2,14 @@ using System;
 using System.Web.Script.Serialization;
 using Amazon.SimpleNotificationService;
 using Amazon.SimpleNotificationService.Model;
-using Ellemy.CQRS.Config;
 using Ellemy.CQRS.Event;
 
 namespace Ellemy.CQRS.Publishing.AmazonSns
 {
-    public static class AmazonConfigurationExtensions
-    {
-        public static AmazonPublisherConfig AmazonPublisher(this Configuration configuration)
-        {
-            return new AmazonPublisherConfig(configuration);
-        }
-    }
-
-    public class AmazonPublisherConfig
-    {
-        private readonly Configuration _configuration;
-
-        public AmazonPublisherConfig(Configuration configuration)
-        {
-            _configuration = configuration;
-        }
-
-
-        internal string AccessKeyId { get; private set; }
-        internal string SecretKey { get; private set; }
-        internal string TopicAccessResourceName { get; private set; }
-
-        public AmazonPublisherConfig AwsAccessKeyId(string accessKeyId)
-        {
-            AccessKeyId = accessKeyId;
-            return this;
-        }
-
-        public AmazonPublisherConfig AwsSecretKey(string awsSecretKey)
-        {
-            SecretKey = awsSecretKey;
-            return this;
-        }
-
-        public AmazonPublisherConfig TopicArn(string value)
-        {
-            TopicAccessResourceName = value;
-            return this;
-        }
-        public Configuration CreatePublisher()
-        {
-            _configuration.PublishEventsWith(new AmazonPublisher(this));
-            return _configuration;
-        }
-    }
-
     public class AmazonPublisher : IEventPublisher
     {
-        private AmazonSimpleNotificationServiceClient _client;
-        private string _topicArn;
+        private readonly AmazonSimpleNotificationServiceClient _client;
+        private readonly string _topicArn;
 
         internal AmazonPublisher(AmazonPublisherConfig config)
         {
@@ -66,7 +19,7 @@ namespace Ellemy.CQRS.Publishing.AmazonSns
        public void Publish<TDomainEvent>(TDomainEvent @event) where TDomainEvent : IDomainEvent
         {
             var serializer = new JavaScriptSerializer();
-            string payload = serializer.Serialize(@event);
+            var payload = serializer.Serialize(@event);
             
             var request = new PublishRequest
                               {

96ace3e35653e4545f445c7880e261a1f2b61b03
Elliott O'Hara <elliott.ohara@gmail.com>
2011-02-17 09:54:42 -0600

    Extracted a TopicSubscriber that handles Queues subscribing to topics

--- Extensions/Ellemy.CQRS.Publishing.AmazonSns/AmazonSqsSubscriber.cs
+++ Extensions/Ellemy.CQRS.Publishing.AmazonSns/AmazonSqsSubscriber.cs
@@ -1,21 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Threading;
 using System.Web.Script.Serialization;
-using Amazon.SimpleNotificationService.Model;
 using Amazon.SQS;
 using Amazon.SQS.Model;
 using Ellemy.CQRS.Event;
-using Attribute = Amazon.SQS.Model.Attribute;
 
 namespace Ellemy.CQRS.Publishing.AmazonSns
 {
     public class AmazonSqsSubscriber
     {
         private readonly AmazonConfig _config;
-        private AmazonSQS _client;
+        private readonly AmazonSQS _client;
       
 
         public AmazonSqsSubscriber(AmazonConfig config)
@@ -28,41 +25,9 @@ namespace Ellemy.CQRS.Publishing.AmazonSns
         private void SetupQueue()
         {
             new QueueManager(_client, _config).SetupQueue();
-            SubscribeToTopic();
+            new TopicSubscriber(_client,_config).Subscribe();
         }
 
-        private void SubscribeToTopic()
-        {
-            SetPermissions();
-            var getArnRequest = new GetQueueAttributesRequest().WithQueueUrl(_config.SqsQueueUrl).WithAttributeName("QueueArn");
-            var clientArn = _client.GetQueueAttributes(getArnRequest).GetQueueAttributesResult.Attribute[0].Value;
-
-            var sns = Amazon.AWSClientFactory.CreateAmazonSNSClient(_config.AccessKeyId,
-                                                                    _config.SecretKey);
-
-            var subscriptionRequest = new SubscribeRequest()
-                .WithEndpoint(clientArn)
-                .WithProtocol("sqs")
-                .WithTopicArn(_config.TopicAccessResourceName);
-
-            sns.Subscribe(subscriptionRequest);
-        }
-
-        private void SetPermissions()
-        {
-            var setQueueAttributeRequest = new SetQueueAttributesRequest()
-               .WithQueueUrl(_config.SqsQueueUrl)
-               .WithAttribute(new Attribute { Name = "Policy", Value = AllowSnsAttribute() });
-            _client.SetQueueAttributes(setQueueAttributeRequest);
-        }
-        private string AllowSnsAttribute()
-        {
-            var attribute = _client.GetQueueAttributes(
-             new GetQueueAttributesRequest().WithQueueUrl(_config.SqsQueueUrl).WithAttributeName("QueueArn"));
-            var queueArn = attribute.GetQueueAttributesResult.Attribute.First().Value;
-            const string format = @"{{""Version"": ""2008-10-17"",""Statement"": [{{""Resource"": ""{0}"", ""Effect"": ""Allow"", ""Sid"": ""1"", ""Action"": ""sqs:*"", ""Condition"": {{""StringEquals"": {{""aws:SourceArn"": ""{1}""}}}}, ""Principal"": {{""AWS"": ""*""}}}}]}}";
-            return String.Format(format, queueArn, _config.TopicAccessResourceName);
-        }
         private bool _stopped;
         public void Stop()
         {
